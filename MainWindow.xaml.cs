9e263b0be7c281f685e13ca9162deafec225544c
Sepehr Noroozi <noroozi.sepehr@gmail.com>
2025-09-25 22:35:53 +0330

    Initial commit: Here the project is initialized.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MyTodoist
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // نمونه داده
+            ListLeft.Items.Add("Item 1");
+            ListLeft.Items.Add("Item 2");
+            ListLeft.Items.Add("Item 3");
+        }
+
+        private void MoveToRight_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ListLeft.SelectedItems;
+            foreach (var item in selectedItems)
+            {
+                ListRight.Items.Add(item);
+            }
+
+            while (ListLeft.SelectedItems.Count > 0)
+            {
+                ListLeft.Items.Remove(ListLeft.SelectedItems[0]);
+            }
+        }
+
+        private void MoveToLeft_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItems = ListRight.SelectedItems;
+            foreach (var item in selectedItems)
+            {
+                ListLeft.Items.Add(item);
+            }
+
+            while (ListRight.SelectedItems.Count > 0)
+            {
+                ListRight.Items.Remove(ListRight.SelectedItems[0]);
+            }
+        }
+    }
+}
